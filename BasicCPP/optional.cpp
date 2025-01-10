82a00ed2704f3f283f2cf6547d3eb0ed2a99cc52
Gabriel <gabrieltroop27@gmail.com>
2025-01-10 19:06:00 -0300

    refatoramento e anotacoes a mais sobre optionals, nullptr e outros funcionamentos

--- BasicCPP/optional.cpp
+++ BasicCPP/optional.cpp
@@ -1,17 +1,29 @@
 #include <optional>
 #include <iostream>
 
-/* Metodo que pode ou não retornar um valor, possuindo assim bool has_value,
-   e value() por si só */
+/* Metodo que retorna um optional podendo ou nao retornar um valor. 
+optionals possuem metodo has_value() e value(), dentre outros. 
+Assim optional.has_value() pode ser utilizado para pegar um bool true caso 
+exista um valor a esse optional, false caso contrario. (Obs: exibir o valor do metodo
+has_value(); vai exibir 1 ou 0 para true ou false).
+Enquanto isso optional.value() pega o valor por si so. 
+Tentar acessar um valor vazio gera excessao std::bad_optional_access */
 std::optional<int> retornaOuNaoUmValor(int int_to_check_if_is_positive)
 {
-    // Retorna optional false caso if seja atendido
-    if (int_to_check_if_is_positive <= -2)
+    // Retorna optional.has_value() = false caso if seja atendido
+    if (int_to_check_if_is_positive <= 0)
     {
-        return {};
+        // return NULL; // retorna null. Obs: null e vazio sao coisas diferentes.
+        return {}; // retorna optional de valor vazio.
+
+        /* atribuir corpos vazios em optionals é equivalente a retornar um bool false
+        para has_value(), e um valor vazio para value(), atencao, tentar acessar um valor vazio
+        de um optional vai causar uma excecao do tipo std::bad_optional_access.
+        retornar null a um optional causara has_value() = true, porem em C++ a representacao
+        de valor null é 0. */
     }
 
-    return int_to_check_if_is_positive;
+    return int_to_check_if_is_positive; // retorna o parametro recebido
 }
 
 int main()
@@ -20,14 +32,23 @@ int main()
     std::cout << "Digite um numero Positivo: ";
     std::cin >> entradaNumber;
     std::optional<int> NumeroOpcional = retornaOuNaoUmValor(entradaNumber);
-
+    
+    // Verifica se o optional nao possui valor
     if (!NumeroOpcional.has_value())
-    {
-        std::cout << "Sem valor positivo: " << NumeroOpcional.has_value();
+    {   
+        std::cout << "Valor de Entrada: " << entradaNumber << "\n";
+        /* Se trocar has_value() por value() na linha abaixo, causara a excecao std::bad_optional_access
+        pois o unico jeito deste if ser atendido é com o optional NAO possuindo valor */
+        std::cout << "Sem valor positivo retornado: " << NumeroOpcional.has_value();
+        std::cout << "\nTamanho em Bytes do retorno: " << sizeof(NumeroOpcional);
+        std::cout << "\nTamanho em Bytes da entrada: " << sizeof(entradaNumber);
     }
-    else 
-    {
-        std::cout << "Foi recebido um valor positivo: " << NumeroOpcional.value();
+    else
+    {   
+        std::cout << "Valor de Entrada: " << entradaNumber << "\n";
+        std::cout << "Foi retornado um valor positivo: " << NumeroOpcional.value();
+        std::cout << "\nTamanho em Bytes do retorno: " << sizeof(NumeroOpcional);
+        std::cout << "\nTamanho em Bytes da entrada: " << sizeof(entradaNumber);
     }
 
     return 0;   
